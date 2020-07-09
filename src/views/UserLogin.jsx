2df62003b47655c8b96b244098fe7002902b33cd
rushmaa <arushma07@gmail.com>
2020-07-10 01:04:51 +1000

    functional login

--- src/views/UserLogin.jsx
+++ src/views/UserLogin.jsx
@@ -1,12 +1,31 @@
 import React, {useState} from "react";
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
 import {Link} from 'react-router-dom';
-
 import "../assets/css/theme.css";
 import { Button } from "reactstrap";
 import { IoIosLock, IoMdPerson } from "react-icons/io";
 
 class UserLogin extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:'',
+      password:''
+    }
+  }
+  login=()=>{
+    fetch(`http://localhost:5000/user/login/`, {
+      method: "post",
+      body: JSON.stringify({ email: this.state.username, pass: this.state.password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+
+      });
+  };
 render() {
 
     // const [toggle, setToggle] = useState(true)
@@ -50,14 +69,14 @@ render() {
               <InputGroupAddon addonType="prepend">
                 <InputGroupText><IoMdPerson/></InputGroupText>
               </InputGroupAddon>
-              <Input placeholder="Username" />
+              <Input placeholder="Username" onChange={(e)=>this.setState({username:e.target.value})}/>
             </InputGroup>
             <div style={{ padding: "3%" }}></div>
             <InputGroup style={{  width: "60%", margin: "auto" }}>
               <InputGroupAddon addonType="prepend">
                 <InputGroupText><IoIosLock/></InputGroupText>
               </InputGroupAddon>
-              <Input placeholder="Password" />
+              <Input placeholder="Password" type={'password'} onChange={(e)=>this.setState({password:e.target.value})}/>
             </InputGroup>
             
           </div>
@@ -66,6 +85,9 @@ render() {
             color="primary"
             className="btn-lg btn-block"
             style={{ width: "49%", height: "10%", margin: "auto"}}
+            onClick={()=>{
+              this.login()
+            }}
           >
             Sign In
           </Button>
