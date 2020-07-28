482dc15bab0d360cbbcef6a03b0c57f64a4f7cbc
Rushma <arushma07@gmail.com>
2020-07-29 00:43:12 +1000

    Revert "Merge branch 'master' into login-final"
    This reverts commit 8e2fe599a235c990036f7d5c86aa8921f18c1b5e, reversing
    changes made to 5c7216bb8a7ce583f272a0fe633582902ce9c50a.

--- src/views/Current.jsx
+++ src/views/Current.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-// import DataTable from "../components/DataTable";
-import TenderDataTable from "../components/TenderDataTable";
+import DataTable from "../components/DataTable";
 import Card from "../components/Card";
 import "../assets/css/theme.css";
 import Title from "../components/Title";
 import MainForm from "../components/Form/MainForm";
 import { TenderTableData } from "../Data/TenderTableData";
 import { connect } from "react-redux";
-import axios from "axios";
 
 const Current = (props) => {
   const [showTable, setShowTable] = React.useState(true);
-  const [searchCode, setsearchCode] = React.useState([]);
-
+  const [searchCode, setsearchCode] = React.useState(TenderTableData);
   React.useEffect(() => {
-    console.log("mounted or updated");
-    console.log(searchCode);
-  });
-  React.useEffect(() => {
-    if (props.User.User.type === "admin") {
+    if (props.User.User.type === 'admin') {
       fetch(`http://localhost:5000/user/getunconfirmeduser`, {
         method: "get",
         headers: {
@@ -27,32 +20,15 @@ const Current = (props) => {
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log("data.users==", data.users);
+          console.log('data.users==', data.users);
           props.setUnconfirmedUserList(data.users);
         });
     }
-    // ! NEW CALL TO DB
-    axios.get("http://localhost:5000/tender/getalltenders").then(
-      (response) => {
-        console.log(response);
-        if (response.data.tenders) {
-          setShowTable(!showTable);
-          setsearchCode(response.data.tenders);
-          setShowTable(showTable);
-        } else {
-          console.log("No data received");
-        }
-      },
-      (error) => {
-        console.log(error);
-        //TODO: show message
-      }
-    );
-  }, []);
+  }, [])
+
 
   const proxy = (searchCode) => {
     console.log(searchCode);
-    console.log("searchcode");
     const results = TenderTableData.filter(
       (element) => element.code === searchCode
     );
@@ -67,8 +43,7 @@ const Current = (props) => {
           title="Current Tenders"
           text="This page shows all open tenders. The list includes all public tenders, as well as selective tenders to which your business can respond. You must be signed in to see selective tenders."
         />
-
-        <div>{showTable && <TenderDataTable searchCode={searchCode} />}</div>
+        <div>{showTable && <DataTable searchCode={searchCode} />}</div>
         <Title header="Search Criteria" />
         <MainForm toggleTable={proxy} />
       </div>
@@ -76,13 +51,12 @@ const Current = (props) => {
   );
 };
 const mapStateToProps = (state) => {
-  var data = { User: state.User };
+  var data = { User: state.User }
   return data;
 };
 const mapDispatchToProps = (dispatch) => {
   return {
-    setUnconfirmedUserList: (data) =>
-      dispatch({ type: "SET_UNCONFIRMED_USER", payload: data }),
+    setUnconfirmedUserList: (data) => dispatch({ type: "SET_UNCONFIRMED_USER", payload: data }),
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Current);
